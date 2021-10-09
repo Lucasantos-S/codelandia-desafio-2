4178b706621ae1ab53d4b76740adb0e8a48d75aa
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-10-09 17:26:29 -0300

    incluido newsletter

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
         stringsElement: '#typed-strings' , 
         typeSpeed: 30 
  });
+
+
+ const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+ });
+
+ScrollReveal().reveal('.container-info', { delay: 200 });
+ScrollReveal().reveal('.destaque', { delay: 400 });
+ScrollReveal().reveal('.title', { delay: 500 });
+ScrollReveal().reveal('.description', { delay: 600 });
